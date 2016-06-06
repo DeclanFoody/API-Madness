a4b648860ae85750ca02b2340ca481dde14aede0
Declan Foody <charles.foody@mail.mcgill.ca>
2016-06-06 17:43:55 +0000

    dirty maps

---
+++ public/javascript/application.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  // <script>
+// function initMap() {
+//   var myLatLng = {lat: 49.2827, lng: -123.1207};
+//   var foofsLatLng = {lat: 53.992, lng: -1.541};
+//   var niamhsLatLng = {lat: 11.562, lng: 104.88};
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: myLatLng,
+//     zoom: 12
+//   });
+// var marker = new google.maps.Marker({
+// position: myLatLng,
+// map: map,
+// draggable: true,
+// animation: google.maps.Animation.DROP,
+// title: 'Dex'
+// });
+// var marker = new google.maps.Marker({
+//   position: foofsLatLng,
+//   map:map,
+//   animation: google.maps.Animation.DROP,
+//   title: 'Foof'
+// });
+// var marker= new google.maps.Marker({
+//   position: niamhsLatLng,
+//   map:map,
+//   animation: google.maps.Animation.DROP,
+//   title: 'Niamh'
+// });
+// // marker.addListener('click', toggleBounce);
+// // }
+// // function toggleBounce(){
+// //   if (marker.getAnimation() !== null){
+// //     marker.setAnimation(null);
+// //   } else {
+// //     marker.setAnimation(google.maps.Animation.BOUNCE);
+// //   }
+
+// var infoWindow = new google.maps.InfoWindow({map:map});
+  
+//   if (navigator.geolocation){
+//    navigator.geolocation.getCurrentPosition(function(position){
+//     var pos = {
+//       lat: position.coords.latitude,
+//       lng: position.coords.longitude
+//     };
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent('Yo foo what you doing over here');
+//     map.setCenter(pos);
+//   },function(){
+//     handleLocationError(true, infoWindow, map.getCenter());
+//   });
+//   } else {
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   }
+// function handleLocationError(browserHasGeoLocation,infoWindow, pos){
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeoLocation ?
+//       'Error: Geolocation service failed' :
+//        'Error: your browser doesnt support GeoLocation');
+// }
+// };
+// </script>
+
+
+//   function initMap(){
+//   var myLatLng = {lat: 49.2827, lng: -123.1207};
+//   var map = new google.maps.Map(
+//     document.getElementById('map'),{
+//       zoom:4
+//       center: myLatLng
+//       });
+//   var marker = new google.maps.Marker({
+//     position: myLatLng
+//     map: map
+//     title: 'Dex'
+  
+//   });
+});
